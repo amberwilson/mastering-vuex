c605cad63d72b708039cffff957b600f8dfe82c2
Amber Wilson <678866+amberwilson@users.noreply.github.com>
2020-04-18 13:58:52 -0300

    Update router.js to use Routes keys

--- src/router.js
+++ src/router.js
@@ -1,29 +1,30 @@
+import Routes from '@/routes';
+import EventCreate from '@/views/EventCreate.vue';
+import EventList from '@/views/EventList.vue';
+import EventShow from '@/views/EventShow.vue';
 import Vue from 'vue';
 import Router from 'vue-router';
-import EventCreate from './views/EventCreate.vue';
-import EventList from './views/EventList.vue';
-import EventShow from './views/EventShow.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'event-list',
+      name: Routes.EventList,
       component: EventList,
     },
     {
-      path: '/event/:id',
-      name: 'event-show',
-      component: EventShow,
-      props: true,
+      path: '/event/create',
+      name: Routes.EventCreate,
+      component: EventCreate,
     },
     {
-      path: '/event/create',
-      name: 'event-create',
-      component: EventCreate,
+      path: '/event/:id',
+      name: Routes.EventShow,
+      component: EventShow,
+      props: true,
     },
   ],
 });
