e076063065b53dd8e30acda2f6a9432359822f01
Amber Wilson <678866+amberwilson@users.noreply.github.com>
2020-04-22 20:43:32 -0300

    Setup store Create Mutations and Actions lookups Create events and mutations for hydrating the store, getting events, and adding an event

--- src/store.js
+++ src/store.js
@@ -1,10 +1,58 @@
+import EventService from '@/services/EventService';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+
 Vue.use(Vuex);
 
+const Mutations = {
+  ADD_EVENT: 'ADD_EVENT',
+  SET_EVENTS: 'SET_EVENTS',
+};
+
+export const Actions = {
+  addEvent: 'addEvent',
+  hydrateStore: 'hydrateStore',
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: { id: 'ab123', name: 'Adam Jahr' },
+    events: [],
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+  },
+  mutations: {
+    [Mutations.ADD_EVENT](state, payload) {
+      state.events.push(payload);
+    },
+    [Mutations.SET_EVENTS](state, payload) {
+      state.events = payload;
+    },
+  },
+  actions: {
+    [Actions.addEvent]({ commit }, event) {
+      return EventService.postEvent(event)
+        .then((response) => {
+          if (response.status === 201) {
+            commit(Mutations.ADD_EVENT, event);
+          } else {
+            console.error('Could not save event', event);
+          }
+        });
+    },
+    [Actions.hydrateStore]({ commit }) {
+      EventService.getEvents()
+        .then((response) => {
+          if (response.status === 200) {
+            commit(Mutations.SET_EVENTS, response.data);
+            return;
+          }
+
+          console.error('Unable to retrieve events.');
+        })
+        .catch((error) => {
+          console.error('There was an error:', error.response);
+        });
+    },
+  },
 });
